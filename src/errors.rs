0dbb80928a2280c0e7b6b0a56c5f477978ef437d
Ravencentric <78981416+Ravencentric@users.noreply.github.com>
2026-02-26 02:56:06 +0530

    fix: remove useless deref

--- src/errors.rs
+++ src/errors.rs
@@ -12,7 +12,7 @@ pub enum FileAttributeKind {
 
 impl std::fmt::Display for FileAttributeKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
+        match self {
             Self::Poster => write!(f, "poster"),
             Self::Date => write!(f, "date"),
             Self::Subject => write!(f, "subject"),
